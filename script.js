b89e52e329be9d48ead1b67f2f251dad720138e4
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-10-28 12:48:40 -0400

    add function to generate QR code

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const apiURL = "http(s)://api.qrserver.com/v1/create-qr-code/?size=10x10&data=";
+const apiURL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
 const qrImage = document.getElementById("qrImage");
-const imgBox = document.getElementById("imgBox");
+let imgBox = document.getElementById("imgBox");
 const qrText = document.getElementById("qrText");
 
 function generateQR() {
   if (qrText.value.length > 0) {
-    qrImage.src = apiURL + qrText.value;
+    qrImage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
 
-    qrImage.classList.add("show-img");
+    imgBox.classList.add("show-img");
   } else {
     qrText.classList.add("error");
     setTimeout(() => {
